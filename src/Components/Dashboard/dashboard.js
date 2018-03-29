bd88a4519a4126916be9e1f0d804df24aed084a7
Dallin Fawcett <dallinfawcett@Dallins-MacBook-Air.local>
2018-03-29 11:39:40 -0600

    yes

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './dashboard.css';
-import { Link } from 'react-router-dom';
 
 
 
@@ -17,22 +16,10 @@ class Dashboard extends Component {
   
 
   render() {
+    console.log(this.props.latitude)
     return (
-      <div className='dashboardMain'>
-        <header className='dashboardHeader'>
-          <Link to="/profile">
-            <button>Profile</button>
-          </Link>
-            Dashboard Header
-        </header>
-        <div className="dashboardBody">
-          <Link to="/friends">
-            <button>Friends</button>
-          </Link>
-          <Link to="/events">
-            <button>Events</button>
-          </Link>
-        </div>
+      <div className="Dashboard">
+        
       </div>
     );
   }
--- src/Components/Landing/landing.js
+++ src/Components/Landing/landing.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import './landing.css';
-import bubbles from './bubbles.jpg'
-
+import Geolocation from "react-geolocation";
+import Dashboard from '../Dashboard/dashboard.js'
 export default class Landing extends Component {
-    render() {
-        
-        return (
-                <section className='landing'>  
-               
-                    <div className='home_box'>
-                      <div className = 'centerbox'>
-                        <div className = "title"> Playdate Mate</div>
-                          <a id="login_button" className='login_button' href={ process.env.REACT_APP_LOGIN }>LOGIN/SIGNUP</a>
-                        </div>
+  constructor(props){
+    super(props);
 
-                    </div>
-                 </section>
-        )
+    this.state ={
+      latitude:'',
+      longitude:''
     }
-
+    this.getLocation = this.getLocation.bind(this);
+  }
+  // componentDidMount(){
+  //   this.getLocation();
+  // }
+  getLocation(){
+    if("geolocation" in navigator){
+      navigator.geolocation.getCurrentPosition(function(position){
+        this.setState({latitude: position.coords.latitude, logitude: position.coords.longitude})
+      })
+    } else{
+      console.log('nope')
+    }
+  }
+    render() {
+      <Dashboard latitude = {this.state.latitude} longitude = {this.state.longitude}/>
+        return (
+          
+          <section className='landing'>  
+          
+               <div className='home_box'>
+                 <div className = 'centerbox'>
+                   <div className = "title"> Playdate Mate</div>
+                     <a id="login_button" className='login_button' href={ process.env.REACT_APP_LOGIN } onClick = {this.getLocation}>LOGIN/SIGNUP</a>
+                   </div>
+               
+               </div>
+            </section>
+       
+                
+        )
+      }
   }
   
 
