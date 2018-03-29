31573875eb18719f834fd0315e9ebb6feab54aab
Dallin Fawcett <dallinfawcett@Dallins-MacBook-Air.local>
2018-03-29 13:34:53 -0600

    yes

--- src/Components/Dashboard/dashboard.js
+++ src/Components/Dashboard/dashboard.js
@@ -17,21 +17,8 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div className='dashboardMain'>
-        <header className='dashboardHeader'>
-          <Link to={`/profile/${this.state.id}`}>
-            <button>Profile</button>
-          </Link>
-            Dashboard Header
-        </header>
-        <div className="dashboardBody">
-          <Link to={`/friends/${this.state.id}`}>
-            <button>Friends</button>
-          </Link>
-          <Link to={`/events/${this.state.id}`}>
-            <button>Events</button>
-          </Link>
-        </div>
+      <div className="Dashboard">
+        
       </div>
     );
   }
