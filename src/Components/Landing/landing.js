39b975360ab62f0536d333eb2a53719cf9b8d307
Dallin Fawcett <dallinfawcett@Dallins-MacBook-Air.local>
2018-03-29 09:33:10 -0600

    updated landing

--- src/Components/Landing/landing.js
+++ src/Components/Landing/landing.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './landing.css';
+import bubbles from './bubbles.jpg'
 
 export default class Landing extends Component {
     render() {
         
         return (
-                <section className='App'>  
+                <section className='landing'>  
                
                     <div className='home_box'>
-
-                        <a id="login_button" className='login_button' href={ process.env.REACT_APP_LOGIN }>LOGIN</a>
+                      <div className = 'centerbox'>
+                        <div className = "title"> Playdate Mate</div>
+                          <a id="login_button" className='login_button' href={ process.env.REACT_APP_LOGIN }>LOGIN/SIGNUP</a>
+                        </div>
 
                     </div>
                  </section>
