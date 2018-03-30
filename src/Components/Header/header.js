307870b516779277794963d3173b72be3a22c037
Dallin Fawcett <dallinfawcett@Dallins-MacBook-Air.local>
2018-03-29 22:31:59 -0600

    updated header on almost every page

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './header.css';
 import {Link} from 'react-router-dom'
 import ProfileIcon from 'react-icons/lib/fa/user';
+import DashboardIcon from 'react-icons/lib/fa/home';
+import BackButton from 'react-icons/lib/fa/chevron-left';
 
 
 
 class Header extends Component {
-  
+  constructor(props){
+    super(props)
+
+    this.state ={
+      id:null
+    }
+  };
   
 
   render() {
@@ -21,7 +29,56 @@ class Header extends Component {
             <button className = 'logout'>logout</button>
               {/* </a> */}</div>
               :
-            <div className = "header-title">Playdate Mate</div>
+              this.props.profile?
+              <div className = "header-body">
+              <Link to = {`/dashboard`}><DashboardIcon size ={30} color ={'white'}/></Link>
+              <div className = "header-title">Profile</div>     
+              {/* <a href = {process.env.REACT_APP_LOGOUT}> */}
+            <button className = 'logout'>logout</button>
+              {/* </a> */}</div>
+              :
+              this.props.events? 
+              <div className = "header-body">
+              <Link to = {`/dashboard`}><DashboardIcon size ={30} color ={'white'}/></Link>
+              <div className = "header-title">Events</div>     
+              {/* <a href = {process.env.REACT_APP_LOGOUT}> */}
+            <button className = 'logout'>logout</button>
+              {/* </a> */}</div>
+              :
+              this.props.searchEvents?
+              <div className = "header-body">
+              <Link to = {`/events/${this.props.searchEventsId}`}><BackButton size ={30} color ={'white'}/></Link>
+              <div className = "header-title">Search Events</div>     
+              {/* <a href = {process.env.REACT_APP_LOGOUT}> */}
+            <button className = 'logout'>logout</button>
+              {/* </a> */}</div>
+              :
+              this.props.CreateEvent?
+              <div className = "header-body">
+              <Link to = {`/events/${this.props.CreateEvent}`}><BackButton size ={30} color ={'white'}/></Link>
+              <div className = "header-title">Create Event</div>     
+              {/* <a href = {process.env.REACT_APP_LOGOUT}> */}
+            <button className = 'logout'>logout</button>
+              {/* </a> */}</div>
+              :
+              this.props.Friends ?
+              <div className = "header-body">
+              <Link to = {'/dashboard'}><DashboardIcon size ={30} color ={'white'}/></Link>
+              <div className = "header-title">Friends</div>     
+              {/* <a href = {process.env.REACT_APP_LOGOUT}> */}
+            <button className = 'logout'>logout</button>
+              {/* </a> */}</div>
+              :
+              this.props.SearchFriends ?
+              <div className = "header-body">
+              <Link to = {`/friends/${this.props.SearchFriends}`}><BackButton size ={30} color ={'white'}/></Link>
+              <div className = "header-title">Find Friends</div>     
+              {/* <a href = {process.env.REACT_APP_LOGOUT}> */}
+            <button className = 'logout'>logout</button>
+              {/* </a> */}</div>
+              :
+
+            null
             }
            
       </div>
