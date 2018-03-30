cff1af29d1830f7b2632b6b2aeb7ceb49b6e1f83
Tyler <tylerkonesky@hotmail.com>
2018-03-30 10:47:55 -0600

    sql tables

--- server/controllers/friends_controller.js
+++ server/controllers/friends_controller.js
@@ -7,6 +7,24 @@ module.exports = {
             console.log(response)
             res.status(200).send(response)
         }).catch('DIDNT WORK!!!')
-    }
+    },
+    addNewFriend: (req, res) =>{
+        const db = req.app.get('db')
+        db.add_new_friend([])
+    },
+    getSenderRequest: (req, res) =>{
+        const db = req.app.get('db')
+        db.get_sender_requests([])
+    },
+    getReceiverRequest: (req, res) =>{
+        const db = req.app.get('db')
+        db.get_receiver_requests([])
+    },
+    acceptFriendRequest: (req, res) =>{
+        const db = req.app.get('db')
+        db.accept_friend_requests([])
+    },
+
+    
 
 }
